960133c67c5311f9cc1a532a53e62ee40d686167
Caio Passos <caio.passos@corp.globo.com>
2017-01-24 16:34:27 -0200

    Lots of updates on middleware structure

--- swift_search/middleware.py
+++ swift_search/middleware.py
@@ -54,6 +54,41 @@ META_OBJECT_PREFIX = 'x-object-meta'
 ALLOWED_HEADERS = ['content-type', 'content-length', 'x-project-name']
 
 
+def start_queue_conn(conf, logger):
+        """
+        If there's a queue channel started, return it immediately.
+        Otherwise, trys to connect to the queue and create the channel.
+
+        :returns: pika.adapters.blocking_connection.BlockingChannel if success;
+                  None otherwise.
+        """
+
+        credentials = pika.PlainCredentials(conf.get('queue_username'),
+                                            conf.get('queue_password'))
+
+        params = pika.ConnectionParameters(
+            host=conf.get('queue_url'),
+            port=int(conf.get('queue_port')),
+            virtual_host=conf.get('queue_vhost'),
+            credentials=credentials
+        )
+
+        try:
+            connection = pika.BlockingConnection(params)
+        except (pika.exceptions.ConnectionClosed, Exception):
+            logger.error('SwiftSearch: Fail to connect to RabbitMQ')
+            return None
+
+        try:
+            channel = connection.channel()
+            channel.queue_declare(queue='swift_search', durable=True)
+        except (pika.exceptions.ConnectionClosed, Exception):
+            logger.exception('SwiftSearch: Fail to create channel')
+            channel = None
+
+        return channel
+
+
 class SwiftSearch(object):
     """
     Swift search middleware
@@ -65,24 +100,23 @@ class SwiftSearch(object):
 
         self.app = app
         self.conf = conf
-        self.queue_channel = None
+        self.queue = None
 
     @swob.wsgify
     def __call__(self, req):
-        self.logger.debug('SwiftSearch called...')
 
         if not self.is_suitable_for_indexing(req):
-            self.logger.debug(
-                'SwiftSearch: %s not suitable for indexing', req.path_info)
             return self.app
 
-        self.queue_channel = self.start_queue_conn()
+        # If queue is None, start connection
+        self.queue = self.queue or start_queue_conn(self.conf, self.logger)
 
-        if self.queue_channel is not None:
+        if self.queue:
             self.send_req_to_queue(req)
         else:
-            self.logger.error('No RMQ connection, skiping indexing for: %s' %
-                              req.path_info)
+            self.logger.error(
+                'SwiftSearch: No RMQ connection, skiping indexing for: %s' %
+                req.path_info)
 
         return self.app
 
@@ -97,118 +131,76 @@ class SwiftSearch(object):
          :param req
          :returns: True if the request is able to indexing; False otherwise.
         """
-        if req.method not in ("PUT", "POST", "DELETE"):
+
+        # Verify method
+        if req.method not in ('PUT', 'POST', 'DELETE'):
             self.logger.debug(
-                'SwiftSearch: %s not suitable for indexing: Invalid method',
-                req.path_info
-            )
+                'SwiftSearch: %s %s not indexable: Invalid method',
+                req.method, req.path_info)
             return False
 
+        # Verify uri
         try:
             vrs, acc, con, obj = req.split_path(2, 4, rest_with_last=True)
         except ValueError:
-            self.logger.debug(
-                'SwiftSearch: %s not suitable for indexing: Fail to parse url',
-                req.path_info
-            )
             # /info or similar...
+            self.logger.debug(
+                'SwiftSearch: %s %s not indexable: Invalid url',
+                req.method, req.path_info)
             return False
 
         # Check if it's an account request or a container request
         if con is None or obj is None:
-            reason = 'container' if obj is None else 'account'
             self.logger.debug(
-                'SwiftSearch: %s not suitable for indexing: %s url',
-                req.path_info,
-                reason
-            )
+                'SwiftSearch: %s %s not indexable: Not a object',
+                req.method, req.path_info)
             return False
 
+        # Verify if container has the meta-search-enabled header
         sysmeta_c = get_container_info(req.environ, self.app)['meta']
-        sysmeta_a = get_account_info(req.environ, self.app)['meta']
-
         enabled = sysmeta_c.get(META_SEARCH_ENABLED)
+
         if enabled is None:
+
+            # Verify if account has the meta-search-enabled header
+            sysmeta_a = get_account_info(req.environ, self.app)['meta']
             enabled = sysmeta_a.get(META_SEARCH_ENABLED)
 
-        self.logger.debug(
-            ('SwiftSearch: %s not suitable for indexing: '
-             'header ``%s`` not found'),
-            req.path_info,
-            META_SEARCH_ENABLED
-        )
+        if not utils.config_true_value(enabled):
+            self.logger.debug(
+                'SwiftSearch: %s %s not indexable: header ``%s`` not found',
+                req.method, req.path_info, META_SEARCH_ENABLED)
 
-        self.logger.debug(
-            'SwiftSearch: Container headers found - {}'.format(sysmeta_c))
-        self.logger.debug(
-            'SwiftSearch: Account headers found - {}'.format(sysmeta_a))
+            self.logger.debug(
+                'SwiftSearch: Container headers found - {}'.format(sysmeta_c))
+            self.logger.debug(
+                'SwiftSearch: Account headers found - {}'.format(sysmeta_a))
 
-        return utils.config_true_value(enabled)
-
-    def start_queue_conn(self):
-        """
-        If there's a queue channel started, return it immediately.
-        Otherwise, trys to connect to the queue and create the channel.
-
-        :returns: pika.adapters.blocking_connection.BlockingChannel if success;
-                  None otherwise.
-        """
-
-        if self.queue_channel is not None:
-            return self.queue_channel
-
-        credentials = pika.PlainCredentials(self.conf.get('queue_username'),
-                                            self.conf.get('queue_password'))
-
-        params = pika.ConnectionParameters(
-            host=self.conf.get('queue_url'),
-            port=int(self.conf.get('queue_port')),
-            virtual_host=self.conf.get('queue_vhost'),
-            credentials=credentials
-        )
-
-        try:
-            connection = pika.BlockingConnection(params)
-        except (pika.exceptions.ConnectionClosed, Exception):
-            self.logger.error('SwiftSearch: Fail to connect to RabbitMQ')
-            return None
-
-        try:
-            channel = connection.channel()
-            channel.queue_declare(queue='swift_search', durable=True)
-        except (pika.exceptions.ConnectionClosed, Exception):
-            self.logger.exception('SwiftSearch: Fail to create channel')
-            channel = None
-
-        return channel
+        return True
 
     def send_req_to_queue(self, req):
-        self.logger.info('SwiftSearch starting thread to send %s to queue' %
+        self.logger.info('SwiftSearch: starting thread to send %s to queue' %
                          req.path_info)
 
-        SwiftSearch.send_thread = SendThread(self.queue_channel,
+        SwiftSearch.send_thread = SendThread(self.queue,
                                              req,
-                                             self.logger)
+                                             self.logger,
+                                             self.conf)
         SwiftSearch.send_thread.start()
 
 
 class SendThread(threading.Thread):
 
-    def __init__(self, queue_channel, req, logger):
+    def __init__(self, queue, req, logger, conf):
         super(SendThread, self).__init__()
         self.req = req
-        self.queue_channel = queue_channel
+        self.queue = queue
         self.logger = logger
+        self.conf = conf
 
     def run(self):
-        self.logger.debug('SwiftSearch: SendThread.run started')
 
-        headers = {}
-        for key in self.req.headers.keys():
-            # We only send allowed headers or ``x-object-meta`` headers
-            if key.lower() in ALLOWED_HEADERS or \
-               key.lower().startswith(META_OBJECT_PREFIX):
-                headers[key] = self.req.headers.get(key)
+        headers = self._filter_headers()
 
         message = {
             'uri': self.req.path_info,
@@ -217,22 +209,50 @@ class SendThread(threading.Thread):
             'timestamp': datetime.datetime.utcnow().isoformat()
         }
 
-        self.logger.debug(message)
+        # self.logger.debug(message)
 
+        result = None
+
+        # First try to send to queue
         try:
-            resp = self.queue_channel.basic_publish(
-                exchange='',
-                routing_key='swift_search', body=json.dumps(message),
-                properties=pika.BasicProperties(delivery_mode=2)
-            )
+            result = self._publish(message)
 
-            if not resp:
-                self.logger.error('SwiftSearch: Fail to send message to queue')
-                self.logger.error(message)
-
-        except Exception:
+        except (pika.exceptions.ConnectionClosed, Exception):
             self.logger.exception('SwiftSearch: Exception on send to queue')
-            self.logger.error(message)
+            self.logger.error(
+                'SwiftSearch: ConnectionClosed to queue. Trying to reconnect')
+
+            # Second try to send to queue
+            self.queue = start_queue_conn(self.conf, self.logger)
+            if self.queue:
+                result = self._publish(message)
+
+        if result:
+            self.logger.info(
+                'SwiftSearch: %s %s sent to queue',
+                self.req.method, self.req.path_info)
+        else:
+            self.logger.error(
+                'SwiftSearch: %s %s failed to send',
+                self.req.method, self.req.path_info)
+
+    def _filter_headers(self):
+        headers = {}
+
+        for key in self.req.headers.keys():
+            # We only send allowed headers or ``x-object-meta`` headers
+            if key.lower() in ALLOWED_HEADERS or \
+               key.lower().startswith(META_OBJECT_PREFIX):
+                headers[key] = self.req.headers.get(key)
+
+        return headers
+
+    def _publish(self, message):
+        return self.queue.basic_publish(
+            exchange='',
+            routing_key='swift_search', body=json.dumps(message),
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
 
 
 def filter_factory(global_conf, **local_conf):
